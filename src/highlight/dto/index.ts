dde46acfdb605c8aaba3ef4305e96793c56400d8
22120358 <phamchithuan777@gmail.com>
2026-02-06 00:15:23 +0700

    feat: add highlight and comment api

---
+++ src/highlight/dto/index.ts
@@ -0,0 +1,7 @@
+// src/highlight/dto/index.ts
+// Barrel export for highlight DTOs
+
+export * from './create-highlight.dto';
+export * from './update-highlight.dto';
+export * from './highlight-response.dto';
+export * from './comment.dto';
